569c717c2a0a0295ba1349a6f684dc93fc0251ae
Pamela Murillo <pamelamurilloa@gmail.com>
2022-09-21 11:19:35 -0600

    The main now calls for the interface

---
+++ TareaVHerencia/src/MainStart/TareaVHerencia.java
@@ -0,0 +1,12 @@
+package MainStart;
+
+import Interfaces.InitialPage;
+
+public class TareaVHerencia {
+
+    public static void main(String[] args) {
+        InitialPage newWindow = new InitialPage( );
+        newWindow.setVisible(true);
+    }
+    
+}
